44435c035ad56c6aabc8a70443ed410a60fbc33e
Eduard Avasilichioaiei <eduard@192-168-0-186.rdsnet.ro>
2022-12-29 19:28:58 +0200

    cart dropdown complete

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   // The toggle function calls the setIsCartFunction which inverts the isCartOpen constant; so when the cart is open and the button is pushed it closes the cart and vice versa
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
@@ -12,7 +12,7 @@ const CartIcon = () => {
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
